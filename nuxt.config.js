a8d950bc7f61d249e49e8ad2ebd3278b1a590cbe
chuenningyeh <chuenningyeh@gmail.com>
2023-11-25 18:36:10 +0800

    add bootstrapVue

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,18 +5,31 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '台北風水設計,風水室內設計,台北空間規劃,台北專業裝潢設計,住宅風水設計,台北室內裝修,台北北歐風設計,台北工業風設計,收納空間配置,收納空間設計,居家風水設計,工業風室內設計,北歐風室內設計,台北居家風水設計' },
-      { name: 'format-detection', content: 'telephone=no' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '台北風水設計,風水室內設計,台北空間規劃,台北專業裝潢設計,住宅風水設計,台北室內裝修,台北北歐風設計,台北工業風設計,收納空間配置,收納空間設計,居家風水設計,工業風室內設計,北歐風室內設計,台北居家風水設計'
+      },
+      {
+        name: 'format-detection',
+        content: 'telephone=no'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.js' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }],
+    script: [{
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.js'
+    }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -25,31 +38,36 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ["bootstrap-vue/nuxt"],
+  bootstrapVue: {
+    icons: true,
+    bootstrapCSS: false,
+    bootstrapVueCSS: false
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     // 圖片轉成base64
-    loaders:
-    {imgUrl:{limit:1000}},
+    loaders: {
+      imgUrl: {
+        limit: 1000
+      }
+    },
     // 後處理器
     // postcss:{preset:{
     //   autoprefixer:{
     //     browsers:["last 1 versions"]
     //   }
     // }},
-    extractCSS:true
+    extractCSS: true
   }
 }
