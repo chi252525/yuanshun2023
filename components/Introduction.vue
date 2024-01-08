42087b67f151a105f32cc3a54eaa5c4f93b357f4
chuenningyeh <chuenningyeh@gmail.com>
2024-01-09 07:59:08 +0800

    update

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -9,17 +9,13 @@
                 <p>室內設計，不僅僅是圖紙、尺寸、材質和工法上的專業，我們需要換位思考，</p>
     
                 <p>深入了解屋主的生活步調、習慣和喜好，加上與屋主的交流，雙方一起激盪獨特的靈感，</p>
-                <div class="d-flex">
-                    <span class="mr-2">我們才能設計出承載屋主獨一無二生命故事的家。</span>
+                <p>我們才能設計出承載屋主獨一無二生命故事的家。&nbsp;
                     <b-button @click="goToAbout" pill variant="outline-secondary" class="m-0">
-                        <span>More</span>
+                        More
                     </b-button>
+                </p>
+                <br>
     
-                </div>
-            </div>
-        </div>
-        <div class="mt-5 mb-5 text-box">
-            <div>
                 <p> 風水與室內設計？</p>
     
                 <p>我們提供獨特的服務是與房屋相關的傳統儀式，</p>
@@ -28,14 +24,10 @@
     
                 <p>我們有一位專業的風水命理老師，可以提供風水諮詢服務，或結合我們的室內設計，</p>
     
-                <div class="d-flex ">
-                    <span>風水與室內設計，兼顧美感又不會突兀，讓風水老師與室內設計師的矛盾不再上演。</span> &nbsp;
-                    <span><b-button @click="goToFengshui" pill variant="outline-secondary"><span>More</span>
+                <p>風水與室內設計，兼顧美感又不會突兀，讓風水老師與室內設計師的矛盾不再上演。&nbsp;
+                    <b-button @click="goToFengshui" pill variant="outline-secondary">More
                     </b-button>
-                    </span>
-                </div>
-    
-    
+                </p>
             </div>
         </div>
     </b-container>
